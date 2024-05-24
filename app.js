02be3bc02ac6e82490cc914c7d611cea8bc1baa4
David <davidkhun2004@gmail.com>
2024-05-24 15:26:34 +0700

    Status: added interactive form usong bootrap
    working on the form that allow user to add book and display them

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const form = document.getElementById('add-form');
+
 const myLibrary = [];
 
-function Book() {
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    addBookToLibrary()
+    form.reset()
+})
+
+function Book(title, author, pages) {
   // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
   // do stuff here
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    
+    const addedBook = new Book(title, author, pages);
+    myLibrary.push(addedBook);
+    console.log(myLibrary)
+}
+
+function displayBooks() {
+    
 }
